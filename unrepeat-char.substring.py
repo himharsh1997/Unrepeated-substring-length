31e617dc127202ab86a9f7314814f32882a47fa0
Himanshu Joshi <himharsh1997@gmail.com>
2020-05-10 00:58:10 +0530

    Update unrepeat-char.substring.py

--- unrepeat-char.substring.py
+++ unrepeat-char.substring.py
@@ -8,15 +8,15 @@ class Solution:
 
     def lengthOfLongestSubstring(self, s):
         while self.index != len(s):
-            self.chrRepeatCheck(s)
-            self.chrNonRepeatCheck(s)
+            self.chrRepeatHandler(s)
+            self.chrNonRepeatHandler(s)
             self.index = self.index + 1
-        self.setMaxLengthStr(self.currentStrLength)
+        self.setMaxLengthStr()
         return self.maxLengthStr
 
     def chrRepeatHandler(self, s):
         if(self.crossedChrDict.get(s[self.index]) != None):
-            self.setMaxLengthStr(self.currentStrLength)
+            self.setMaxLengthStr()
             self.index = self.crossedChrDict.get(s[self.index]) + 1
             self.crossedChrDict = {}
             self.currentStrLength = 0
@@ -26,7 +26,7 @@ class Solution:
             self.crossedChrDict[s[self.index]] = self.index
             self.currentStrLength = self.currentStrLength + 1
 
-    def setMaxLengthStr(self, currentStrLength):
+    def setMaxLengthStr(self):
         if(self.maxLengthStr < self.currentStrLength):
             self.maxLengthStr = self.currentStrLength
 
